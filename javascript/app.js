a81e895f9014bf09e3b8f499782f3bfaef9cc547
Phi Hai Nguyen <phihai93@gmail.com>
2019-10-21 13:27:40 -0700

    update

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,9 @@
-getData();
+createChart();
+
 async function getData() {
+  const yearData = [];
+  const tempData = [];
+
   const response = await fetch("ZonAnn.Ts+dSST.csv");
   //async/awaits have built in method to recieve and get raw text called .text()
   const data = await response.text();
@@ -14,8 +18,52 @@ async function getData() {
   table.forEach(row => {
     const columns = row.split(",");
     const year = columns[0];
+    yearData.push(year);
     const temp = columns[1];
+    tempData.push(parseFloat(temp) + 14);
     console.log(year, temp);
   });
   //   console.log(rows);
+  // By returning our data as objects we will be able to access that object when calling it in the createChart() method
+  return { yearData, tempData };
+}
+
+//=============CODE FOR CHART/TABLE================//
+
+async function createChart() {
+  //by making createChart() an async function and calling getData() as an await function, the createChart function will wait until the data is recieved before loading the chart
+  const data = await getData(); //by assigning the getData() method to a variable we are then able to get those data which we returned as an object
+  const ctx = document.getElementById("myChart").getContext("2d");
+
+  const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: data.yearData, //this is how we call the objects from what we returned earlier
+      datasets: [
+        {
+          label:
+            "Combined Land-Sruface Air and Sea-Surface Water temperature(C°)",
+          data: data.tempData,
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+          fill: false
+        }
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              // Include a dollar sign in the ticks
+              callback: function(value, index, values) {
+                return value + " C°";
+              }
+            }
+          }
+        ]
+      }
+    }
+  });
 }
